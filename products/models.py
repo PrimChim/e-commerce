c5683e552864667af308d8b18243a634d520ae46
PrimChim <gpritam922@gmail.com>
2024-04-09 15:01:42 +0545

    commenting feature added

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
@@ -11,3 +12,12 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+
+class ProductLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.user.username} likes {self.product.name}'
--- users/views.py
+++ users/views.py
@@ -1,5 +1,54 @@
 from django.shortcuts import render
+from products.models import Product, ProductLike
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 # Create your views here.
+@login_required
 def home(request):
-    return render(request, 'users/index.html')
+    
+    products = Product.objects.all()
+
+    # add a new key likes to each product
+    for product in products:
+        product.likes = ProductLike.objects.filter(product=product).count()
+
+    # sort products by likes
+    products = sorted(products, key=lambda x: x.likes, reverse=True)
+
+    context = {
+        'products': products
+    }
+
+    if request.method == 'POST':
+        user = request.user
+        data = request.POST
+        product_id = data['product_id']
+        product = Product.objects.get(id=product_id)
+
+        already_liked = ProductLike.objects.filter(product=product, user=user).exists()
+        if not already_liked:
+            like = ProductLike.objects.create(product=product, user=user)
+            like.save()
+        else:
+            ProductLike.objects.filter(product=product, user=user).delete()
+
+
+    return render(request, 'users/index.html', context)
+
+def login_view(request):
+    if request.method == 'POST':
+        data = request.POST
+        email = data['email']
+        password = data['password']
+
+        user = authenticate(username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('users:home')
+        else:
+            return render(request, 'users/login.html', {'error': 'Invalid credentials'})
+
+    return render(request, 'users/login.html')
